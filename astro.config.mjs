4b523ca2ed94ceaae4278fc814cf21499bab64f4
soumame <sou.tokumaru@gmail.com>
2024-01-23 21:45:21 +0800

    Astro.urlの重複を解消。これでデプロイできるはず！

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import nodejs from "@astrojs/node";
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
